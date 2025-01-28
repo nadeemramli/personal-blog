f55d3d5dd9118635c837636ec0ee087683d741f8
nadeemramli <m.nadeemramli@gmail.com>
2025-01-28 22:11:15 +0800

    .

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,14 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.MobileOnly(Component.Explorer({
+      title: "Explore",
+      folderClickBehavior: "collapse",
+      folderDefaultState: "collapsed",
+      useSavedState: true,
+    })),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
